2870092f27b5863ed932fec498cc1c84a1163623
ShubhamModi004 <shubhammodi004@gmail.com>
2019-02-11 01:50:18 +0530

    Added Netflify Email Indentifieer

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,64 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import netlifyIdentity from 'netlify-identity-widget';
 import "./layout.css"
 
 
 import gatsbyLogo from '../images/gatsby-icon.png';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+class Header extends React.Component {
 
-      {/* Title and Logo */}
-      <span style={{display: 'flex' , alignItems: 'center'}}>
-      <img src={gatsbyLogo} alt="Gatsby garb Logo" style={{ 
-        width: '50px',
-        border: '3px solid orange',
-        margin: '0 5px',
-        borderRadius: '50%'
-        }}/>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          className="blazing"
-          getProps={({ isCurrent }) => {
-            return isCurrent ? { className: "active" } : null
+  componentDidMount() {
+    netlifyIdentity.init();
+  }
+
+  render() {
+    const { siteTitle } = this.props; 
+    return(
+      (
+        <header
+          style={{
+            background: `rebeccapurple`,
+            marginBottom: `1.45rem`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-      </span>
-    </div>
-  </header>
-)
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `1.45rem 1.0875rem`,
+            }}
+          >
+
+            {/* Title and Logo */}
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={gatsbyLogo} alt="Gatsby garb Logo" style={{
+                width: '50px',
+                border: '3px solid orange',
+                margin: '0 5px',
+                borderRadius: '50%'
+              }} />
+              <h1 style={{ margin: 0 }}>
+                <Link
+                  to="/"
+                  className="blazing"
+                  getProps={({ isCurrent }) => {
+                    return isCurrent ? { className: "active" } : null
+                  }}
+                >
+                  {siteTitle}
+                </Link>
+              </h1>
+              <div data-netlify-identity-menu></div>
+            </span>
+          </div>
+        </header>
+      )
+    )
+  }
+  
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
