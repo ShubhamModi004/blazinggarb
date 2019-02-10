0219d40f481c7aebbb2976e629a3051bd14bfa71
ShubhamModi004 <shubhammodi004@gmail.com>
2019-02-11 02:01:24 +0530

    test to see the User Name

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql, Link } from "gatsby"
+import netlifyIdentity from 'netlify-identity-widget';
 
 import Header from "./header"
 import "./layout.css"
@@ -17,49 +18,57 @@ const getSiteMetadata = graphql`
       }
 `
 
-const Layout = ({ children }) => (
+class Layout extends React.Component() {
   
-  <StaticQuery
-    query={getSiteMetadata}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <ul style={{ display: 'flex', borderBottom: '1px solid', listStyle: 'none' }}>
-          <li style={{ padding: '3px', margin: '10px'}}><Link getProps={({ isCurrent }) => {
-              return isCurrent ? { className: "active" } : null
-            }} to="/" >Home</Link></li>
-          <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
-              return isCurrent ? { className: "active" } : null
-            }} to="/page-2">Page 2</Link></li>
-          <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
-              return isCurrent ? { className: "active" } : null
-            }} to="/page-3">Page 3</Link></li>
-          <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
-              return isCurrent ? { className: "active" } : null
-            }} to="/blog">Blog</Link></li>
-          <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
-            return isCurrent ? { className: "active" } : null
-          }} to="/products">Products</Link></li>
-        </ul>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="http://shubham004.in/intelictoWebsite">{data.site.siteMetadata.author}</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+  render() {
+    const { children } = this.props;
+    const user = netlifyIdentity.currentUser();
+    return (  
+      <StaticQuery
+        query={getSiteMetadata}
+        render={data => (
+          <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <ul style={{ display: 'flex', borderBottom: '1px solid', listStyle: 'none' }}>
+              <li style={{ padding: '3px', margin: '10px'}}><Link getProps={({ isCurrent }) => {
+                  return isCurrent ? { className: "active" } : null
+                }} to="/" >Home</Link></li>
+              <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
+                  return isCurrent ? { className: "active" } : null
+                }} to="/page-2">Page 2</Link></li>
+              <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
+                  return isCurrent ? { className: "active" } : null
+                }} to="/page-3">Page 3</Link></li>
+              <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
+                  return isCurrent ? { className: "active" } : null
+                }} to="/blog">Blog</Link></li>
+              <li style={{ padding: '3px', margin: '10px' }}><Link getProps={({ isCurrent }) => {
+                return isCurrent ? { className: "active" } : null
+              }} to="/products">Products</Link></li>
+            </ul>
+            
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                paddingTop: 0,
+              }}
+            >
+              <main>{children}</main>
+              <footer>
+                © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="http://shubham004.in/intelictoWebsite">{data.site.siteMetadata.author}</a>
+              </footer>
+            </div>
+          </>
+        )}
+      />
+    )
+  }
+}
+
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
